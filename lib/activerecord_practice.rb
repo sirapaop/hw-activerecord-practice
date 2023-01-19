2e36bba5ec4f2085d1805d4ff9383cda716ad508
sirapaop <sirapaop2545@gmail.com>
2023-01-19 21:16:08 +0700

    ActiveRecord

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -10,6 +10,7 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 class Customer < ActiveRecord::Base
   def to_s
+
     "  [#{id}] #{first} #{last}, <#{email}>, #{birthdate.strftime('%Y-%m-%d')}"
   end
 
@@ -19,9 +20,59 @@ class Customer < ActiveRecord::Base
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    where(first: 'Candice')
+    
   end
+  
   def self.with_valid_email
-    # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+  	
+    	where("email LIKE ?", "%@%")
   end
+  
+  def self.with_dot_org_email
+  	where("email LIKE ?", "%.org%")
+  end
+  def self.with_invalid_email
+    	where("email NOT LIKE ?", "%@%")
+  end
+  
+  def self.with_blank_email
+  	where(email: nil)
+  end
+  
+  def self.born_before_1980
+  	where("birthdate < ?", "1980-01-01")
+  end
+  
+  def self.with_valid_email_and_born_before_1980
+  	where("email LIKE ? AND birthdate < ?", "%@%", "1980-01-01" ) 
+  end
+  
+  def self.last_names_starting_with_b
+  	where("last LIKE ?", "B%").order(birthdate: 'asc')
+  end
+  
+  def self.twenty_youngest
+  	order(birthdate: :desc).limit(20)
+  	
+  end
+  
+  def self.update_gussie_murray_birthdate
+  	where(first: 'Gussie').update(birthdate: '2004-02-08')
+  end
+  
+  def self.change_all_invalid_emails_to_blank
+  	where("email != ' ' AND email NOT NULL AND email NOT LIKE ?", "%@%").update(email: nil)
+  end
+  
+  def self.delete_meggie_herman
+  	find_by(:first => 'Meggie', :last => 'Herman').destroy
+  end
+  
+  def self.delete_everyone_born_before_1978
+  	where("birthdate < ?", "1978-01-01").destroy_all
+  end
+  
+ 
   # etc. - see README.md for more details
 end
